aa1ad984179ec6cee5924fee7fa226cb5f6624dd
gibum1228 <gibum1228@naver.com>
2019-05-09 12:37:56 +0900

    3D 비행기 움직이는 게임 만들기
    2019.05.09.

---
+++ UnityAirplane/Assets/Script/Airplane.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    public GameObject airplane;
+    Transform tr; // 포지션, 로케이션, 스케일 관리하는 변수 이름 Transform
+
+    /* void Awake() // 처음에 생성되기 전에 한 번만 실행되는 문
+    {
+    } */
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = airplane.GetComponent<Transform>(); // tr에 airplane transform 컴퍼넌트를 가져온다
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            tr.position = new Vector3(tr.position.x, tr.position.y, tr.position.z + 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            tr.position = new Vector3(tr.position.x - 1, tr.position.y, tr.position.z);
+            tr.rotation = Quaternion.Euler(tr.position.x - 10, 0, tr.position.z + 10);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            tr.position = new Vector3(tr.position.x, tr.position.y, tr.position.z - 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            tr.position = new Vector3(tr.position.x + 1, tr.position.y, tr.position.z);
+        }
+    }
+}
---
+++ UnityAirplane/Assets/Script/AirplaneCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneCamera : MonoBehaviour
+{
+    public GameObject airplane;
+    public GameObject airplaneCamera;
+
+    Transform tr;
+    Transform trCm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = airplane.GetComponent<Transform>();
+        trCm = airplaneCamera.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trCm.transform.position = Vector3.Lerp(trCm.transform.position, tr.transform.position + new Vector3(0,8,-15), 0.3f); // trcm에서 tr까지 1초안에 이동시킴
+    }
+}
