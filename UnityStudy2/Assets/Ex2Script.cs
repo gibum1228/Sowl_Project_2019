618c79a2b8da551570f25a84800e2ee267aacae2
gibum1228 <gibum1228@naver.com>
2019-05-19 14:42:12 +0900

    버튼 클릭시 Text 출력 및 Time 이용하기

---
+++ UnityStudy2/Assets/Ex2Script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex2Script : MonoBehaviour
+{
+    public GameObject ready_t;
+    public GameObject steady_t;
+    public GameObject Bang_t;
+    public GameObject win_t;
+    public GameObject lose_t;
+    
+    float time = 0f; // 시간 초기화
+    bool play = true; // 게임 가능 여부 판단
+
+    // Update is called once per frame
+    void Update() // 무한 반복문
+    {   if (play == true)
+        {
+            time += Time.deltaTime; // 엔진이 한 바퀴 도는 작동의 시간을 저장(프로그램 시작하고 시간 재기)
+        }
+
+        if(time > 1f && time < 2f) // 초마다 알맞은 텍스트 출력
+        {
+            ready_t.SetActive(true);
+        }else if(time > 2f && time < 3.5f)
+        {
+            ready_t.SetActive(false);
+        }
+        else if (time > 3.5f && time < 4.5f)
+        {
+            steady_t.SetActive(true);
+        }
+        else if (time > 4.5f && time < 6f)
+        {
+            steady_t.SetActive(false);
+        }else if(time > 6f && time < 7f)
+        {
+            Bang_t.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0)) // 마우스 클릭시
+            {
+                win_t.SetActive(true);
+                Bang_t.SetActive(false);
+                play = false;
+                time = 0;
+            }
+        }else if(time > 7)
+        {
+            Bang_t.SetActive(false);
+            lose_t.SetActive(true);
+            play = false;
+        }
+    }
+}
---
+++ UnityStudy2/Assets/UIButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIButton : MonoBehaviour
+{
+    public GameObject goldText;
+    public GameObject showText; // Text와 연결시켜 주기 위해 선언
+
+    Text _goldText; // 텍스트 래퍼런스형 변수 (컴퍼넌트를 접근하기 위함)
+
+    bool text_OnOff = false; // text 활성화 여부 판단하는 변수
+    int gold = 0; // 골드량 변수
+
+    public void showBox() // 버튼 누를 시 이벤트
+    {
+        if (text_OnOff == false){ // 꺼져있으면 키고
+            showText.SetActive(true); // true는 활성화시키기, false는 비활성화시키기
+            text_OnOff = true;
+        }else if(text_OnOff == true){ // 커져있으면 끄고
+            showText.SetActive(false); // true는 활성화시키기, false는 비활성화시키기
+            text_OnOff = false;
+        }
+    }
+    
+    public void miningGold()
+    {
+        _goldText = goldText.GetComponent<Text>();
+        gold += 5;
+        _goldText.text = gold.ToString() + "g"; // 골드는 int형이기 떄문에 String으로 바꾼 후 _goldText 변경
+    }
+}
