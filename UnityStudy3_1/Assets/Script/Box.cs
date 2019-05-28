e6d10127464f0d9668e5965d024d040f25e228a6
gibum1228 <gibum1228@naver.com>
2019-05-28 13:41:28 +0900

    물리를 이용해 중력을 표현하기

---
+++ UnityStudy3_1/Assets/Script/Box.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{   
+    void OnTriggerEnter2D(Collider2D cl)    // 서로 겹쳤을 때
+    {
+        if(cl.tag == "Box")
+        {
+            Debug.Log("충돌했음");
+        }
+    }
+
+    void OnCollisionEnters(Collider2D cl)    // 부딪혔을 때
+    {
+
+    }
+}
