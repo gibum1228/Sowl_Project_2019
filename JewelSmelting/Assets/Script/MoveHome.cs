df41c0b0a733fcd69514ea7523cf0cb56c4fe77e
gibum1228 <gibum1228@naver.com>
2019-05-29 12:26:43 +0900

    보석 강화하기 게임

---
+++ JewelSmelting/Assets/Script/MoveHome.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveHome : MonoBehaviour
+{
+    public GameObject HomeBtn;
+
+    public void moveHome()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ JewelSmelting/Assets/Script/MoveScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    public GameObject WorkBtn;
+    public GameObject JuBtn;
+    public GameObject JcBtn;
+    public GameObject AbillityBtn;
+    public GameObject InventoryBtn;
+
+    public void moveWorkChoice()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void moveJU()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void moveJC()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void moveAbillity()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void moveInven()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+}
