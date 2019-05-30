c0c6423b61bc83b483e727047a758d0eee47e57e
gibum1228 <gibum1228@naver.com>
2019-05-30 12:33:38 +0900

    아이템 담을 인벤토리 만들기

---
+++ inventoryProject/Assets/Script/Item.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum TYPE { Weopon, Jewel, Scroll, Use } // 무기, 보석, 주문서, 소비템
+
+    public int itemID;          // 아이템 고유번호
+    public string itemName;         // 아이템 이름
+    public string itemDes;          // 아이템 설명
+    public int itemPower;           // 아이템 공격력
+    public int itemLevel = 1;           // 아이템 강화 정도
+    public TYPE itemType;           // 아이템의 타입.
+    public Sprite DefaultImg;   // 기본 이미지.
+    public int MaxCount;        // 겹칠수 있는 최대 숫자.  
+
+    // 인벤토리에 접근하기 위한 변수.
+    private Inventory Iv;
+
+    void Awake()
+    {
+        // 태그명이 "Inventory"인 객체의 GameObject를 반환한다.
+        // 반환된 객체가 가지고 있는 스크립트를 GetComponent를 통해 가져온다.
+        Iv = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+
+    }
+
+    public Item() { }
+
+    public Item(int id, string name, string des, int power, TYPE type, int count)
+    {
+        this.itemID = id;
+        this.itemName = name;
+        this.itemDes = des;
+        this.itemPower = power;
+        this.itemType = type;
+        this.MaxCount = count;
+    }
+
+
+    void AddItem()
+    {
+        // 아이템 획득에 실패할 경우.
+        if (!Iv.AddItem(this))
+            Debug.Log("아이템이 가득 찼습니다.");
+        else // 아이템 획득에 성공할 경우.
+            gameObject.SetActive(false); // 아이템을 비활성화 시켜준다.
+    }
+
+    // 충돌체크
+    void OnTriggerEnter(Collider _col)
+    {
+        // 플레이어와 충돌하면.
+        if (_col.gameObject.layer == 10)
+            AddItem();
+    }
+}
+
---
+++ inventoryProject/Assets/Script/UpgradeItem.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeItem : MonoBehaviour
+{
+    public List<Item> itemUpgrade = new List<Item>();           // 업그레이드 할 아이템 정보 담을 itemUpgrade, 한 개만 담을 수 있음
+
+    public GameObject UpgradeInven;         // 업그레이드 할 아이템 올릴 창, Upgrade_Back
+    public GameObject UpgradeBtn;           // 업그레이드 버튼
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void upgradeItem()           // 업그레이드 버튼 클릭시
+    {
+        // 업그레이드 할 아이템 받아오기
+        if (Slot.slot.Pop())            //  UpgradeInven에 Pop()이 실행이 되면 -> 참, 아마도 참인지 거짓인지만 검사?
+        {
+            itemUpgrade.Add(Slot.slot.Pop());           // itemgrade에 Pop()으로 값 반환 후 UpgradeInven에서 제거시킴
+        }
+        else
+        {
+            // 아이템 올려달라는 문구 출력
+            Debug.Log(12412412);
+        }
+
+        // 강화 확률
+        if(itemUpgrade[0].itemLevel <= 5)
+        {
+            if(Random.Range(1,101) <= 70)
+            {
+                itemUpgrade[0].itemLevel += 1;
+            }
+        }
+        if (itemUpgrade[0].itemLevel <= 10)
+        {
+            if (Random.Range(1, 101) <= 50)
+            {
+                itemUpgrade[0].itemLevel += 1;
+            }
+        }
+        if (itemUpgrade[0].itemLevel <= 15)
+        {
+            if (Random.Range(1, 101) <= 30)
+            {
+                itemUpgrade[0].itemLevel += 1;
+            }
+            else
+            {
+                itemUpgrade[0].itemLevel -= 1;
+            }
+        }
+        if (itemUpgrade[0].itemLevel <= 15)         // 최대 16강까지 가능
+        {
+            if (Random.Range(1, 101) == 1)
+            {
+                itemUpgrade[0].itemLevel += 1;
+            }
+            else if(Random.Range(1, 101) == 100)
+            {
+                itemUpgrade.RemoveAt(0);
+            }
+            else
+            {
+                itemUpgrade[0].itemLevel -= 1;
+            }
+        }
+    }
+}
