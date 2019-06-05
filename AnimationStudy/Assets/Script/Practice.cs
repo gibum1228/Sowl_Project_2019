30c853281bff55f86f2e29932dd5b090b184aaf8
gibum1228 <gibum1228@naver.com>
2019-06-05 17:15:31 +0900

    애니메이터를 이용해 조건에 따라 큐브에 애니메이션 주기

---
+++ AnimationStudy/Assets/Script/Practice.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+
+    bool __a = true;
+    float _time = 0;
+
+    void Start()
+    {
+
+       
+    }
+
+    void Update()
+    {
+
+        _time += Time.smoothDeltaTime;
+
+        if(_time > 1f && __a == true)
+        {
+            __a = false;
+            if (IsInvoking("gibum") == false)
+            {
+                Invoke("gibum", 1);
+            }
+
+        }
+    }
+    void gibum()
+    {
+          
+    }
+
+    IEnumerator aa()
+    {
+        yield return 0;
+    }
+}
---
+++ AnimationStudy/Assets/Script/cubeAniamtior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeAniamtior : MonoBehaviour
+{
+    Animator Anim;
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        StartCoroutine(anims());
+    }
+
+    IEnumerator anims()
+    {
+        yield return new WaitForSeconds(3);
+        Anim.SetBool("gibum",true);
+
+
+        yield return 0;
+    }
+}
